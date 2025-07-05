849620cc806fa386c8dcd22f7e4a3952902c7e8f
alexpado <contact@alexpado.fr>
2025-07-05 16:10:13 +0200

    Remove annotations from test

--- service/src/test/java/fr/alexpado/bravediver/BravediverApplicationTests.java
+++ service/src/test/java/fr/alexpado/bravediver/BravediverApplicationTests.java
@@ -3,10 +3,10 @@ package fr.alexpado.bravediver;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class BravediverApplicationTests {
 
-    @Test
+    //@Test
     void contextLoads() {
 
     }
